7aae6f31b3dd0aef2711c844115e5ee0a2aa9074
Daniel Kwapien <danikwapien@gmail.com>
2021-12-11 16:21:46 +0100

    Start button and settings button

---
+++ src/components/Start.svelte
@@ -0,0 +1,15 @@
+<div id="start-button" class="justify-around">
+    <button class="text-4xl py-4 px-10 text-white font-bold shadow-indigo-500/50">
+        Start
+    </button>
+</div>
+
+
+<style>
+    #start-button{
+        margin-top: 5vh;
+        background-color: #6266F1;
+        border-radius: 20px;
+    }
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script>
-    
+    import Start from "../components/Start.svelte";
 </script>
 
 
@@ -13,16 +13,12 @@
         <h2 class="text-2xl font-bold p-10">Break time</h2>
     </div>
         <div id="outer" class="bg-color justify-center place-content-center">
-            
             <svg id="timer-container" class="flex -rotate-90 justify-center content-center" xmlns="http://www.w3.org/2000/svg">
-                <circle id="circle-stop"  class="shadow" stroke-width="20" cx="50%" cy="50%" r="215" fill="transparent" >
-                </circle>
+                <circle id="circle-stop"  class="shadow" stroke-width="20" cx="50%" cy="50%" r="215" fill="transparent" />
             </svg>
             <p id="time" class="relative text-7xl  font-extrabold">25:00</p>
         </div>
-        
-    
-    
+        <Start class="my-72"></Start>
 </main>
 
 <style>
@@ -36,10 +32,10 @@
     #circle-stop{
         height: auto;
         width: auto;
-        stroke: #7D53DE;
+        stroke: #6266F1;
         stroke-linecap: round;
         stroke-dasharray: 1500;
-        stroke-dashoffset: 1200;
+        stroke-dashoffset: 1250;
     }
     #outer{
         width: 450px;
@@ -51,17 +47,16 @@
         box-shadow: inset 0px 0px 6px 0px #E2E2E2;
     }
     circle{
-   width:150px;height:150px;
-   border: solid 1px #555;
-   background-color: #eed;
-   box-shadow: 10px -10px rgba(0,0,0,0.6);
-   -moz-box-shadow: 10px -10px rgba(0,0,0,0.6);
-   -webkit-box-shadow: 10px -10px rgba(0,0,0,0.6); 
-   -o-box-shadow: 10px -10px rgba(0,0,0,0.6);
-   border-radius:100px;
+        width:150px;height:150px;
+        border: solid 1px #555;
+        background-color: #eed;
+        box-shadow: 10px -10px rgba(0,0,0,0.6);
+        -moz-box-shadow: 10px -10px rgba(0,0,0,0.6);
+        -webkit-box-shadow: 10px -10px rgba(0,0,0,0.6); 
+        -o-box-shadow: 10px -10px rgba(0,0,0,0.6);
+        border-radius:100px;
     }
     #time{
         transform: translateY(-16.5rem);
     }
-
 </style>
